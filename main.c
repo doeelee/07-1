02988f5fc9bdbefd72d3ede117c375beccf98e0f
doelele <doelele@users.noreply.github.com>
2020-10-15 15:58:38 +0900

    07-4

--- main.c
+++ main.c
@@ -2,22 +2,23 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int inc(int counter)
-{
-	counter++;
-	return counter;
-}
 
-
+void f(void);
+int i;
 
 int main(int argc, char *argv[]) {
 	
-	int i=0;
-	
-	printf("before function call i = %d\n", i);
-	inc(i);
-	printf("after function call i = %d\n", i);
-	
+	for(i=0;i<5;i++)
+	{
+		f();
+	}
+
 	
 	return 0;
+}
+
+void f(void)
+{
+	for(i=0;i<10;i++)
+	    printf("#");
 }
